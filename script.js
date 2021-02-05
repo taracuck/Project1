b9b0d262c8c8777e75da477a0c12033f554ef014
taracuck <taracuck@umich.edu>
2021-02-05 15:11:30 -0700

    final day 3 edits

--- script.js
+++ script.js
@@ -1,70 +1,69 @@
 "use strict";
-
-// trying to randomly assign picture to card
 let board = document.querySelector(".board");
 let resetButton = document.querySelector(".reset-button");
 let starterDiv = document.querySelector(".starter-div");
-let startButton = document.querySelector(".start-button");
-let homeTab = document.querySelector(".home");
+let startButton = document.querySelector(".button");
+let generalTab = document.querySelector(".general");
 let theOfficeTab = document.querySelector(".office");
+let objectsTab = document.querySelector(".objects");
+let superheroesTab = document.querySelector(".superheroes");
 let clickCounter = document.createElement("array");
-clickCounter = [];
-
 let defaultImageSources = [
   {
-    src: "assets/anna.jpeg",
+    src: "assets/general/anna.jpeg",
     id: 1,
   },
   {
-    src: "assets/bert.jpeg",
+    src: "assets/general/bert.jpeg",
     id: 2,
   },
   {
-    src: "assets/buzz.jpg",
+    src: "assets/general/buzz.jpg",
     id: 3,
   },
   {
-    src: "assets/chewbacca.jpg",
+    src: "assets/general/chewbacca.jpg",
     id: 4,
   },
   {
-    src: "assets/elsa.jpg",
+    src: "assets/general/elsa.jpg",
     id: 1,
   },
   {
-    src: "assets/ernie.jpg",
+    src: "assets/general/ernie.jpg",
     id: 2,
   },
   {
-    src: "assets/han-solo.jpg",
+    src: "assets/general/han-solo.jpg",
     id: 4,
   },
   {
-    src: "assets/luigi.jpg",
+    src: "assets/general/luigi.jpg",
     id: 5,
   },
   {
-    src: "assets/mario.jpg",
+    src: "assets/general/mario.jpg",
     id: 5,
   },
   {
-    src: "assets/morty.jpg",
+    src: "assets/general/morty.jpg",
     id: 6,
   },
   {
-    src: "assets/rick.jpg",
+    src: "assets/general/rick.jpg",
     id: 6,
   },
   {
-    src: "assets/woody.jpg",
+    src: "assets/general/woody.jpg",
     id: 3,
   },
 ];
-
 let seconds = 45;
 let countdownTimer;
 let finalCountdown = false;
 let cardsRemoved = 0;
+let currentlyBeingPlayed = defaultImageSources;
+clickCounter = [];
 
 const gameTimer = () => {
   if (seconds >= 10) {
@@ -74,42 +73,30 @@ const gameTimer = () => {
     document.querySelector(".game-timer").innerHTML = `:0${seconds}`;
   }
   if (seconds === 0) {
+    document.querySelector(".game-timer").innerHTML = `:00`;
     let endDiv = document.createElement("div");
     endDiv.classList.add("end-div");
-    let gameOverParagraph = document.createElement("p");
-    gameOverParagraph.classList.add("popUp-p");
-    gameOverParagraph.textContent = "YOU LOST :(";
-    gameOverParagraph.style.color = "white";
-
+    let gameOverImg = document.createElement("img");
+    gameOverImg.src = "assets/dynamic-duo-loser-01.png";
+    gameOverImg.classList.add("game-over-img");
     let gameOverButton = document.createElement("button");
-    gameOverButton.classList.add("start-button");
+    gameOverButton.classList.add("button");
     gameOverButton.textContent = "Try Again?";
     let gameOverPopUp = document.createElement("div");
     gameOverPopUp.classList.add("game-over");
-    gameOverPopUp.append(gameOverParagraph, gameOverButton);
+    gameOverPopUp.append(gameOverImg, gameOverButton);
     endDiv.append(gameOverPopUp);
     document.body.append(endDiv);
     gameOverButton.addEventListener("click", () => {
-      display(defaultImageSources);
-      clearInterval(gameTimer);
+      display(currentlyBeingPlayed);
       document.querySelector(".game-timer").style.color = "white";
       seconds = 45;
       endDiv.style.display = "none";
-      let cardsRemoved = 0;
       gameTimer();
     });
   }
   seconds--;
 };
-
-startButton.addEventListener("click", () => {
-  starterDiv.style.display = "none";
-  clearInterval(gameTimer);
-  document.querySelector(".game-timer").style.color = "white";
-  seconds = 45;
-  countdownTimer = setInterval(gameTimer, 1000);
-  let cardsRemoved = 0;
-});
 
 // shuffle function
 const shuffle = (images) => {
@@ -130,7 +117,7 @@ const shuffle = (images) => {
 };
 
 const assignPictures = (imageSources) => {
-  let cardsRemoved = 0;
+  cardsRemoved = 0;
   board.innerHTML = "";
   imageSources = shuffle(imageSources);
   for (let i = 0; i < imageSources.length; i++) {
@@ -144,16 +131,12 @@ const assignPictures = (imageSources) => {
     // back of card
     let cardBackDiv = document.createElement("div");
     cardBackDiv.classList.add("flip-card-back");
-
     let cardBack = document.createElement("img");
     cardBack.classList.add("card-back");
     cardBack.classList.add("image");
     cardBack.style.backgroundImage = `url(assets/dynamic-duo-text-01.png)`;
     cardBack.style.backgroundPosition = "center";
     cardBack.style.backgroundSize = "cover";
-    // cardBack.id += "back";
-    // cardBack.setAttribute("side", "back");
-
     cardBack.id = imageSources[i].id;
     cardBackDiv.append(cardBack);
     // flip card inner - holds front and back of cards
@@ -168,11 +151,7 @@ const assignPictures = (imageSources) => {
 };
 
 board.addEventListener("click", (e) => {
-  if (
-    // e.target.classList.contains("image") &&
-    e.target.classList.contains("card-back") &&
-    clickCounter.length < 2
-  ) {
+  if (e.target.classList.contains("card-back") && clickCounter.length < 2) {
     e.target.parentNode.parentNode.classList.add("flipped");
     clickCounter.push(e.target);
     if (clickCounter.length === 2) {
@@ -183,21 +162,24 @@ board.addEventListener("click", (e) => {
           cardsRemoved += 2;
           if (cardsRemoved === 12) {
             let endDiv = document.createElement("div");
-            endDiv.classList.add("end-div");
+            endDiv.classList.add("game-over");
+            endDiv.style.display = "flex";
+            let gameOverTimeP = document.createElement("p");
+            gameOverTimeP.textContent = `You finished with ${seconds} seconds to spare!`;
+            gameOverTimeP.style.color = "white";
             let gameOverImg = document.createElement("img");
             gameOverImg.src = "assets/dynamic-duo-winner-01.png";
+            gameOverImg.classList.add("game-over-img");
             let gameOverButton = document.createElement("button");
-            gameOverButton.classList.add("start-button");
+            gameOverButton.classList.add("button");
             gameOverButton.textContent = "Try Again?";
-            let gameOverPopUp = document.createElement("div");
-            gameOverPopUp.classList.add("game-over");
-            gameOverPopUp.append(gameOverImg, gameOverButton);
-            endDiv.append(gameOverPopUp);
+            let gameOverInfoDiv = document.createElement("div");
+            gameOverInfoDiv.append(gameOverTimeP, gameOverButton);
+            endDiv.append(gameOverImg, gameOverInfoDiv);
             document.body.append(endDiv);
-            clearInterval(gameTimer);
+            clearInterval(countdownTimer);
             gameOverButton.addEventListener("click", () => {
-              display(defaultImageSources);
-              clearInterval(gameTimer);
+              display(currentlyBeingPlayed);
               document.querySelector(".game-timer").style.color = "white";
               seconds = 45;
               endDiv.style.display = "none";
@@ -217,14 +199,24 @@ board.addEventListener("click", (e) => {
   }
 });
 
+// start button event listener
+startButton.addEventListener("click", () => {
+  starterDiv.style.display = "none";
+  document.querySelector(".game-timer").style.color = "white";
+  seconds = 45;
+  currentlyBeingPlayed = defaultImageSources;
+  countdownTimer = setInterval(gameTimer, 1000);
+});
+
+// reset button event listener
 resetButton.addEventListener("click", () => {
-  display(defaultImageSources);
-  clearInterval(gameTimer);
+  display(currentlyBeingPlayed);
   document.querySelector(".game-timer").style.color = "white";
   seconds = 45;
   gameTimer();
 });
 
+// office tab event listener
 theOfficeTab.addEventListener("click", () => {
   let OfficeImageSources = [
     {
@@ -276,22 +268,142 @@ theOfficeTab.addEventListener("click", () => {
       id: 6,
     },
   ];
+  currentlyBeingPlayed = OfficeImageSources;
   display(OfficeImageSources);
-  clearInterval(gameTimer);
   document.querySelector(".game-timer").style.color = "white";
   seconds = 45;
   gameTimer();
 });
 
-homeTab.addEventListener("click", () => {
+// objects tab event listener
+objectsTab.addEventListener("click", () => {
+  let objectsImageSources = [
+    {
+      src: "assets/objects/eraser.jpg",
+      id: 1,
+    },
+    {
+      src: "assets/objects/hammer.jpg",
+      id: 2,
+    },
+    {
+      src: "assets/objects/key.jpg",
+      id: 3,
+    },
+    {
+      src: "assets/objects/lock.jpg",
+      id: 3,
+    },
+    {
+      src: "assets/objects/nail.jpg",
+      id: 2,
+    },
+    {
+      src: "assets/objects/pencil.jpeg",
+      id: 1,
+    },
+    {
+      src: "assets/objects/remote.jpeg",
+      id: 4,
+    },
+    {
+      src: "assets/objects/toilet.jpg",
+      id: 5,
+    },
+    {
+      src: "assets/objects/toothbrush.jpeg",
+      id: 6,
+    },
+    {
+      src: "assets/objects/toothpaste.jpeg",
+      id: 6,
+    },
+    {
+      src: "assets/objects/tp.jpg",
+      id: 5,
+    },
+    {
+      src: "assets/objects/tv.jpg",
+      id: 4,
+    },
+  ];
+  currentlyBeingPlayed = objectsImageSources;
+  display(objectsImageSources);
+  document.querySelector(".game-timer").style.color = "white";
+  seconds = 45;
+  gameTimer();
+});
+
+// objects tab event listener
+superheroesTab.addEventListener("click", () => {
+  let superheroesImageSources = [
+    {
+      src: "assets/superheroes/antman1.jpg",
+      id: 1,
+    },
+    {
+      src: "assets/superheroes/batman.jpg",
+      id: 2,
+    },
+    {
+      src: "assets/superheroes/catwoman1.jpg",
+      id: 2,
+    },
+    {
+      src: "assets/superheroes/harley-quinn1.jpg",
+      id: 3,
+    },
+    {
+      src: "assets/superheroes/ironman1.jpg",
+      id: 4,
+    },
+    {
+      src: "assets/superheroes/lois-lane.jpg",
+      id: 5,
+    },
+    {
+      src: "assets/superheroes/mary-jane.jpg",
+      id: 6,
+    },
+    {
+      src: "assets/superheroes/pepper-potts1.jpg",
+      id: 4,
+    },
+    {
+      src: "assets/superheroes/spiderman.jpg",
+      id: 6,
+    },
+    {
+      src: "assets/superheroes/superman.jpg",
+      id: 5,
+    },
+    {
+      src: "assets/superheroes/the-joker.jpg",
+      id: 3,
+    },
+    {
+      src: "assets/superheroes/the-wasp1.jpg",
+      id: 1,
+    },
+  ];
+  currentlyBeingPlayed = superheroesImageSources;
+  display(superheroesImageSources);
+  document.querySelector(".game-timer").style.color = "white";
+  seconds = 45;
+  gameTimer();
+});
+
+// general tab event listener
+generalTab.addEventListener("click", () => {
+  currentlyBeingPlayed = defaultImageSources;
   display(defaultImageSources);
-  clearInterval(gameTimer);
   document.querySelector(".game-timer").style.color = "white";
   seconds = 45;
   gameTimer();
 });
 
 const display = (imageSources) => {
+  clearInterval(gameTimer);
   assignPictures(imageSources);
 };
 
